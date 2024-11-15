5bcc4843a42bc51796b08668c5d4a91eb744162b
Shams-Joha <shamsjoha3@gmail.com>
2024-11-15 18:24:39 +0600

    implement register page functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,35 +1,52 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
+
+    const { user, setUser } = useContext(AuthContext);
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        // 
+        const name = e.target.name.value;
+        const photo = e.target.photo.value;
+        const password = e.target.password.value;
+        const email = e.target.email.value;
+        
+
+
+    }
+
     return (
         <div className="min-h-screen flex justify-center items-center">
 
             <div className="card bg-base-100 w-full max-w-lg shadow-none shrink-0 rounded-none p-10">
                 <h2 className="text-2xl font-semibold text-center">Register your Account</h2>
-                <form className="card-body">
-                <div className="form-control">
+                <form onSubmit={handleOnSubmit} className="card-body">
+                    <div className="form-control">
                         <label className="label">
                             <span className="label-text font-semibold">Your Name</span>
                         </label>
-                        <input type="text" placeholder="Enter your Name" className="input input-bordered" required />
+                        <input name="name" type="text" placeholder="Enter your Name" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-semibold">Photo Url</span>
                         </label>
-                        <input type="text" placeholder="Enter your Photo Url" className="input input-bordered" required />
+                        <input name="photo" type="text" placeholder="Enter your Photo Url" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-semibold">Email Address</span>
                         </label>
-                        <input type="email" placeholder="Enter your email" className="input input-bordered" required />
+                        <input name="email" type="email" placeholder="Enter your email" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-semibold">Password</span>
                         </label>
-                        <input type="password" placeholder="Enter your password" className="input input-bordered" required />
+                        <input name="password" type="password" placeholder="Enter your password" className="input input-bordered" required />
                         <label className="label">
                             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                         </label>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,11 +1,27 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 // Step 1 Create context
 export const AuthContext = createContext();
 
+// Step 7 Now we can access the main component as children of AuthProvider
+const AuthProvider = ({ children }) => {
 
-const AuthProvider = () => {
-    // Step 2 Return AuthContext Provider
-    return <AuthContext.Provider></AuthContext.Provider>
+    // Step 3 Declare state for storing data.
+    const [user, setUser] = useState(null);
+    // Step 4 Declare Object to pass data through context
+    const authInfo = {
+        user,
+        setUser
+    }
+
+
+    // Step 2 Return AuthContext Provider  Step 5 pass object as value.
+    return <AuthContext.Provider value={authInfo}>
+        {/* Step 6  go to main component & wrap main component with AuthProvider*/}
+        {/* Now add this children here */}
+        {
+            children
+        }
+    </AuthContext.Provider>
 };
 
 export default AuthProvider;
